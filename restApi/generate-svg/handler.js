105ddab66b507da5cd3a5b9af7fbba47190fd149
zallek <nicolas.fortin37@gmail.com>
2016-06-17 17:50:22 +0200

    Make the lambda returns errors when fails

--- restApi/generate-svg/handler.js
+++ restApi/generate-svg/handler.js
@@ -5,11 +5,17 @@ const spawn = require('child_process').exec;
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
+  if (!event.chartOptions) {
+    return context.done('BadRequest: no chartOptions');
+  }
+
   const ps = spawn(
     'node node_modules/node-googlecharts/bin/node-googlecharts \'' + JSON.stringify(event) + '\'',
     (error, stdout, stderr) => {
       if (error) {
-        context.done(error);
+        context.done(new Error('Fail: ' + error.message));
+      } else if (stderr) {
+        context.done(new Error('Fail: ' + stderr));
       } else {
         context.done(null, stdout);
       }
