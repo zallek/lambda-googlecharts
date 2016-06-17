3629536c7c48156b91f74aafa64b06b138a9dab0
zallek <nicolas.fortin37@gmail.com>
2016-06-17 14:59:15 +0200

    Make it works on local

--- restApi/generate-svg/handler.js
+++ restApi/generate-svg/handler.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const nodeGoogleCharts = require('node-googlecharts');
-const icu = require('full-icu');
+const spawn = require('child_process').exec;
 
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
-
-  nodeGoogleCharts(event)
-  .then(svg => context.done(null, svg))
-  .catch(err => context.done(err));
+  const ps = spawn(
+    'node node_modules/node-googlecharts/examples/ColumnChart.js',
+    (error, stdout, stderr) => {
+      context.done(null, stdout);
+    }
+  );
 };
