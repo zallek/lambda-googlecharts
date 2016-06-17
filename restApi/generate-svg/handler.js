681c164f9f44e9a784d43fe9854675901889a9f1
zallek <nicolas.fortin37@gmail.com>
2016-06-17 15:12:31 +0200

    Use event input

--- restApi/generate-svg/handler.js
+++ restApi/generate-svg/handler.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const spawn = require('child_process').exec;
+const icu = require('full-icu');
 
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
   const ps = spawn(
-    'node node_modules/node-googlecharts/examples/ColumnChart.js',
+    'node node_modules/node-googlecharts/bin/node-googlecharts \'' + JSON.stringify(event) + '\'',
     (error, stdout, stderr) => {
-      context.done(null, stdout);
+      if (error) {
+        context.done(error);
+      } else {
+        context.done(null, stdout);
+      }
     }
   );
 };
