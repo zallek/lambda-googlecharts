fa2f4179349b4c0ad13f618beaa36d7353d32934
zallek <nicolas.fortin37@gmail.com>
2016-06-17 19:48:53 +0200

    Fix encoding issues on lambda

--- restApi/generate-svg/handler.js
+++ restApi/generate-svg/handler.js
@@ -6,16 +6,16 @@ const spawn = require('child_process').exec;
 // Lambda Handler
 module.exports.handler = function(event, context) {
   if (!event.chartOptions) {
-    return context.done('BadRequest: no chartOptions');
+    return context.done('[BadRequest] no chartOptions');
   }
 
   const ps = spawn(
     'node node_modules/node-googlecharts/bin/node-googlecharts \'' + JSON.stringify(event) + '\'',
     (error, stdout, stderr) => {
       if (error) {
-        context.done(new Error('Fail: ' + error.message));
+        context.done(new Error('[ServerError] ' + error.message));
       } else if (stderr) {
-        context.done(new Error('Fail: ' + stderr));
+        context.done(new Error('[ServerError] ' + stderr));
       } else {
         context.done(null, stdout);
       }
